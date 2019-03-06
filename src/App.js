6bbd682e2346780508d1f192156da68c232a16be
klkirstyleeskl <k.lees@btinternet.com>
2019-03-06 13:20:20 +0000

    adds game container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import GameContainer from './containers/GameContainer';
 
 class App extends Component {
   render() {
     return (
-     null
+      <GameContainer/>
     );
   }
 }
---
+++ src/containers/GameContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+class GameContainer extends Component{
+
+
+
+    render(){
+        return(
+            <div className="game-container">
+            <h1>Tic Tac Toe</h1>
+
+            </div>
+        )
+    }
+
+
+
+}
+
+
+
+export default GameContainer;
